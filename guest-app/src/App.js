b9882942b1cc6db71bbaee03535bf5968b1b4945
jozanini <jozanini@cisco.com>
2024-09-06 13:36:00 -0700

    added switch functionality

--- guest-app/src/App.js
+++ guest-app/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <h1>Guest App</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
